4686c799cbf3a10e846cadafe1817f3c007fb1a9
Kristine <magic.rooter@gmail.com>
2019-11-20 21:10:51 +0300

    Правки в баглисте (+9 позиций)

--- source/js/scrolltop.js
+++ source/js/scrolltop.js
@@ -1,59 +1,71 @@
 /* begin Up-Down button  */
 
-'use strict';
+"use strict";
 
-var upDownBtn = document.querySelector('.promo__scroll');
+var upDownBtn = document.querySelector(".promo__scroll");
 var check;
+var advantagesSection = document.querySelector('.advantages');
 
 function trackScroll() {
-var scrolled = window.pageYOffset;
-var coords = document.documentElement.clientHeight;
+  var scrolled = window.pageYOffset;
+  var coords = document.documentElement.clientHeight;
 
-if (scrolled > coords) {
-     upDownBtn.classList.add('promo__scroll-show');
-    
+  if (scrolled > coords) {
+    upDownBtn.classList.add("promo__scroll-show");
+
     check = false;
   }
-    if (scrolled === 0) {
+  if (scrolled === 0) {
+    check = true;
+  }
+}
+
+function scrollTo(elem) {
+  const offsetTop = elem.getBoundingClientRect().top;
   
+  window.scrollBy(0, offsetTop);
+  
+}
 
-      check = true;
-    }
+function backToTop() {
+  upDownBtn.classList.add("promo__scroll-disabled");
+  if (!check) {
+    (function goTop() {
+      if (window.pageYOffset !== 0) {
+        window.scrollBy(0, -80);
+        setTimeout(goTop, 0);
+      } else {
+        upDownBtn.classList.remove("promo__scroll-disabled");
+      }
+    })();
+    return;
+  } else if (check) {
+    
+    scrollTo(advantagesSection);
+    
+    var match = Math.ceil(
+      window.pageYOffset + document.documentElement.clientHeight
+    );
+
+    upDownBtn.classList.remove("promo__scroll-disabled");
+//     (function goBottom() {
+//       var match = Math.ceil(
+//         window.pageYOffset + document.documentElement.clientHeight
+//       );
+
+//       if (match != document.documentElement.scrollHeight) {
+//         console.log('here', match);
+//         window.scrollBy(0, 80);
+//         setTimeout(goBottom, 0);
+//       } else {
+//         upDownBtn.classList.remove("promo__scroll-disabled");
+//       }
+//     })();
+    return;
   }
+}
 
-  function backToTop() {
-    upDownBtn.classList.add('promo__scroll-disabled');
-    if (!check) {
-      (function goTop() {
-
-        if (window.pageYOffset !== 0) {
-          window.scrollBy(0, -80);
-          setTimeout(goTop, 0);
-        } else {
-          upDownBtn.classList.remove('promo__scroll-disabled');
-        }
-
-      })();
-      return;
-
-    } else if (check) {
-      (function goBottom() {
-        var match = Math.ceil(window.pageYOffset + document.documentElement.clientHeight);
-
-        if (match != document.documentElement.scrollHeight) {
-          window.scrollBy(0, 80);
-          setTimeout(goBottom, 0);
-        } else {
-          upDownBtn.classList.remove('promo__scroll-disabled');
-        }
-
-      })();
-      return;
-    }
-
-  }
-
-  window.addEventListener('scroll', trackScroll);
-upDownBtn.addEventListener('click', backToTop);
+window.addEventListener("scroll", trackScroll);
+upDownBtn.addEventListener("click", backToTop);
 
 /* end Up-Down button  */
