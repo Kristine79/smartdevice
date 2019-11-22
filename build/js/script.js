3741e2afaf8f32262a7335ddc69d198f1ec5ecd9
Kristine <magic.rooter@gmail.com>
2019-11-22 17:57:40 +0300

    Небольшие правки

--- build/js/script.js
+++ build/js/script.js
@@ -91,7 +91,31 @@ textarea.addEventListener('input', function() {
   }
 });
 
+//скролл
 
+var anchors = [].slice.call(document.querySelectorAll('a[href*="#"]'));
+var animationTime = 600;
+var framesCount = 60;
+
+
+anchors.forEach(function (item) {
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    var coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+
+    var scroller = setInterval(function () {
+      var scrollBy = coordY / framesCount;
+
+      if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+        window.scrollBy(0, scrollBy);
+      } else {
+        window.scrollTo(0, coordY);
+        clearInterval(scroller);
+      }
+    }, animationTime / framesCount);
+  });
+});
 
 
 
